2e1998aa2988e19a2cbf3f3289a1d42bcfd045e5
Nirmal <jesintha.nirmal@accenture.com>
2024-04-01 14:36:25 +0530

    tf backend changed with tf cloud org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Jeffy"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "jeffy-github-action-demo"
     }
   }
 }
